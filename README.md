4bd9de704abe7413eda7c1a88322c4dfd87b8ac7
Srivatsan Srinivasan <sriv1999@gmail.com>
2019-02-27 00:05:52 -0500

    Final Commit

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # Challenge Problem
 
-The purpose of this program is to create a function that takes a wikipedia page and answer as input and return a real value that corresponds to how likely the answer is to be correct. The implementation was primarily done through means of scraping each wikipedia page summary (using the wikipedia api) and processing each word and document to be applied to tf-idf.
+The purpose of this program is to create a function that takes a wikipedia page and an answer as input and return a real value that corresponds to how likely the answer is to be correct. The implementation was primarily done through means of scraping each wikipedia page summary (using the wikipedia api) and processing each word and document in order to be applied to the term frequency - inverse document frequency (tf-idf) approach.
 
 
-To call the function simply input your parameters of the wikipedia page and answer as string in find_likeliness()
+To call the function simply input your parameters of the wikipedia page and answer as a string in find_likeliness(). You must have the wikipedia api installed and the dataset downloaded to the appropriate name ("question_data.json").
 
 ```
 print find_likeliness('page', 'answer')
 ```
 
-There also exist two preprocessing methods. The first preprocessing method creates a dictionary mapping of every word in all documents in the Qanta dataset with its document frequency (in how many documents does the word appear), term_frequency (how many times the word appears in each document), and the weight (a word's weight in its respective document calculated using tf-idf).
+There also exist two preprocessing methods.
+
+The first preprocessing method ``` preprocessing() ``` creates a dictionary mapping of every word in all the documents in the Qanta dataset with its document frequency (how many documents does the word appear in), term frequency (how many times the word appears in each document), and the weight (a word's weight in its respective document calculated using tf-idf).
+
+The counter in the first preprocessing method, i, was used to speed up testing time by only accumulating data on the first i documents. The counter is not commented out as it will significantly speed up the testing of this program as less data will not need to be processed. However, the limitation is that when a user answers a question to a wikipedia page not yet processed, the find_likeliness() function will default to a real value of 0.0. So, if you know that the wikipedia page for your answer will be processed within the first 100 pages, the counter has been set to finish at 100 pages in order to obtain an answer quickly without having to go through the entire dataset.
+
+The second preprocessing method ``` preprocessing2('answer') ``` creates a dictionary mapping of only the words in the answer field given by the user to the same as above (document frequency, term frequency, and weights) for an optimized yet not thorough approach.
+
+This program could be significantly optimized by using numpy and nltk, but I used a more general and standard approach to show the crawling, processing, and mapping process more discretely.
--- challenge.py
+++ challenge.py
@@ -2,36 +2,53 @@ import re
 import math
 import json
 import wikipedia
-# Crawl Part
+
+# Crawl and upload of QANTA question dataset
 with open('question_data.json') as f:
     data = json.load(f)
 
-#URL = 'https://en.wikipedia.org/wiki/{url}'
 all_docs = {}
 doc_frequency = {}
 term_frequency = {}
 weights = {}
 
+'''
+This function processes all the words from all wikipedia pages
+in the QANTA question dataset and creates the appropriate dictionaries
+of all_docs (wiki page name -> summary),
+doc_frequency (word -> how many documents word is found in),
+term_frequency (wiki page name -> word -> how many times word is found in this wiki page),
+weights (wiki page name -> word -> weight of word developed by tf-idf formula).
+'''
 def preprocessing():
+    # README gives more details :
+    # The i represents a counter to limit how many documents will be processed
     i = 0
     for q in data['questions']:
-        if i == 25:
+        if i == 100:
             break;
         wiki_page = q['page']
         i += 1
+
+        # Try catch statement to avoid unknown or erroneous wikipedia pages
         try:
+            # Using wikipedia API to get summary and create mapping after cleaning text
             summary = wikipedia.summary(wiki_page).strip().lower()
+
+            # Remove punctuation, convert unicode to string, lower case
             summary = re.sub(r'[^\w\s]', '', summary)
             wiki_page = wiki_page.lower().encode('utf-8')
             all_docs[wiki_page] = summary
         except:
             pass
 
+    # Create dictionaries for every word found in every wiki page in dataset
     for doc in all_docs:
         text = all_docs[doc].split()
         text = [word.encode('utf-8') for word in text]
         term_frequency[doc] = {}
         weights[doc] = {}
+
         for word in text:
             if word not in doc_frequency:
                 doc_frequency[word] = doc_count(word, all_docs)
@@ -40,6 +57,10 @@ def preprocessing():
             if word not in weights[doc]:
                 weights[doc][word] = weight(word, doc)
 
+'''
+This function is nearly identical to the first function except it only processes
+and looks for the words given by the user's answer.
+'''
 def preprocessing2(answer):
     for q in data['questions']:
         wiki_page = q['page']
@@ -54,6 +75,8 @@ def preprocessing2(answer):
     for doc in all_docs:
         term_frequency[doc] = {}
         weights[doc] = {}
+
+        # Instead of all words in all documents, only the answer's words are mapped
         for word in answer.split():
             if word not in doc_frequency:
                 doc_frequency[word] = doc_count(word, all_docs)
@@ -62,32 +85,53 @@ def preprocessing2(answer):
             if word not in weights[doc]:
                 weights[doc][word] = weight(word, doc)
 
+'''
+This function counts the number of documents a given word appears in
+'''
 def doc_count(word, doc_list):
     result = 0
     for doc in doc_list:
         text = all_docs[doc].split()
+
         if word in text:
             result += 1
+
     return result
 
+'''
+This function counts the number of appearances of a word in a given document
+'''
 def frequency_count(word, doc):
     return all_docs[doc].lower().split().count(word.lower())
 
+'''
+This function applies the tf-idf formula in order to determine the weights
+of the words in a given document respective of the dataset
+'''
 def weight(word, doc):
     return frequency_count(word, doc) * math.log10(len(all_docs) / doc_frequency[word])
 
+'''
+This function, after completion of preprocessing, will take in an answer and
+a wikipedia page and return a real value denoting the likeliness that the answer
+is correct. The lowest score an answer can receive is a 0.0.
+'''
 def find_likeliness(page, answer):
-    preprocessing2(answer)
+    preprocessing()
+    #preprocessing2(answer)
     result = 0.0;
+
+    # Computing the sum of (every word's frequency  * its weight) in a given page
     for word in answer.lower().split():
         weight = 0.0
         tf = 0.0
+
         if page.lower() in weights and word in weights[page.lower()]:
             weight = weights[page.lower()][word]
         if page.lower() in term_frequency and word in weights[page.lower()]:
             tf = term_frequency[page.lower()][word]
         result += weight * tf
+
     return result
 
-preprocessing()
-print find_likeliness('Queequeg', 'Queequeg')
+print find_likeliness('Maine', 'Maine')
